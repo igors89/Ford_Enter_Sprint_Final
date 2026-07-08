0d6aad9a45e429b5e05accee31ae99ea828b1fb9
Igor Andrade Soares <igsoares89@gmail.com>
2026-07-08 09:29:50 -0300

    commit automatico

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CabecalhoComponent } from '../cabecalho/cabecalho.component';
 import { RodapeComponent } from '../rodape/rodape.component';
+import { HeroComponent } from '../hero/hero.component';
 
 @Component({
   selector: 'app-main',
-  imports: [CabecalhoComponent,RodapeComponent],
+  imports: [CabecalhoComponent,HeroComponent,RodapeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
