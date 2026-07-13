5869129345a39e70fc0c780cb7f1ab40d79afa86
Igor Andrade Soares <igsoares89@gmail.com>
2026-07-13 10:46:35 -0300

    sync

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CardsComponent } from '../cards/cards.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [CardsComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 import { CabecalhoComponent } from '../cabecalho/cabecalho.component';
 import { RodapeComponent } from '../rodape/rodape.component';
 import { HeroComponent } from '../hero/hero.component';
-import { CardsComponent } from '../cards/cards.component';
+// import { CardsComponent } from '../cards/cards.component';
 import { CarouselComponent } from '../carousel/carousel.component';
 
 @Component({
   selector: 'app-main',
-  imports: [CabecalhoComponent,HeroComponent,RodapeComponent,ReviewsComponent,CarouselComponent,CardsComponent],
+  imports: [CabecalhoComponent,HeroComponent,RodapeComponent,ReviewsComponent,CarouselComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
