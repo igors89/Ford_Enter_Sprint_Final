958dcb226e76767de089723ec7a835e1a60616b4
Igor Andrade Soares <igsoares89@gmail.com>
2026-07-14 00:50:45 -0300

    Commit automático

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { ApiReviews, Review } from '../../models/review';
+import { ReviewService } from '../../services/review.service';
 
 @Component({
   selector: 'app-carousel',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.css'
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit{
+  
+    constructor(private reviewServ: ReviewService){}
+
+    gruposAvaliacoes: Review[][] = [];
+
+    ngOnInit(): void {
+        this.carregarReviews();
+    }
+
+    agruparAvaliacoes(data: Review[]) {
+        const grupo = 3; 
+        this.gruposAvaliacoes = []; // Limpa grupos antigos
+
+        for (let i = 0; i < data.length; i += grupo) {
+            this.gruposAvaliacoes.push(data.slice(i, i + grupo));
+        }
+    }
+
+    carregarReviews(): void {
+        this.reviewServ.getReviews().subscribe((avaliacoes: ApiReviews) => {
+            this.agruparAvaliacoes(avaliacoes.reviews);
+        });
+    }
 
 }
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 import { CabecalhoComponent } from '../cabecalho/cabecalho.component';
 import { RodapeComponent } from '../rodape/rodape.component';
 import { HeroComponent } from '../hero/hero.component';
-// import { CardsComponent } from '../cards/cards.component';
 import { CarouselComponent } from '../carousel/carousel.component';
+import { EspecialidadesComponent } from '../especialidades/especialidades.component';
 
 @Component({
   selector: 'app-main',
-  imports: [CabecalhoComponent,HeroComponent,RodapeComponent,ReviewsComponent,CarouselComponent],
+  imports: [CabecalhoComponent,HeroComponent,RodapeComponent,ReviewsComponent,CarouselComponent,EspecialidadesComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
