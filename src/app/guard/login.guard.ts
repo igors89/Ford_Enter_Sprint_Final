a8cabfa58986f864908d7b700952a41041619754
Igor Andrade Soares <igsoares89@gmail.com>
2026-07-13 23:57:55 -0300

    Commit automático

--- src/app/guard/login.guard.ts
+++ src/app/guard/login.guard.ts
@@ -1,5 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
 export const loginGuard: CanActivateFn = (route, state) => {
+
+  const email = sessionStorage.getItem('email');
+  const router = inject(Router);
+
+  if(!email) {
+    alert('Usuário não autenticado');
+    router.navigate(['']);
+    return false;
+  }
+
   return true;
 };
