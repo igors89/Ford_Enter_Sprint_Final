934564fa5400d4ac51402d3498c87c812d5854f0
Igor Andrade Soares <igsoares89@gmail.com>
2026-07-14 23:54:48 -0300

    Commit automático

--- src/app/services/cadastro.service.ts
+++ src/app/services/cadastro.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Paciente } from '../models/paciente';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CadastroService {
 
-  constructor() { }
+    private readonly API = "http://localhost:3001";
+
+    constructor(private http: HttpClient) { }
+
+    getPacientes(): Observable<{ pacientes: Paciente[] }> {
+        return this.http.get<{ pacientes: Paciente[] }>(`${this.API}/pacientes`);
+    }
+
+    postPaciente(paciente: Omit<Paciente, 'id'>): Observable<{ message: string, novoPaciente: Paciente }> {
+        return this.http.post<{ message: string, novoPaciente: Paciente }>(`${this.API}/paciente`, paciente);
+    }
 }
